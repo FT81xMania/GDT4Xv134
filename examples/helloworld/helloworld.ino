c0801ef815ca157c34ea124ecc810d536fb479de
FT81x Manía <ft81xmania@gmail.com>
2021-03-03 22:56:36 -0600

    Add files via upload

--- examples/helloworld/helloworld.ino
+++ examples/helloworld/helloworld.ino
@@ -1,1 +1,15 @@
+#include <GDXT4X.h>
 
+void setup()
+{
+  Serial.begin(1000000);
+  GD.begin();
+}
+
+void loop()
+{
+  GD.ClearColorRGB(0x500050);
+  GD.Clear();
+  GD.cmd_text(GD.w / 2, GD.h / 2, 31, OPT_CENTER, "Hello world GDXT4X");
+  GD.swap();
+}
