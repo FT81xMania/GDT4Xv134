9dbdddfe6d3fa8b5c5feb4c5b97b38c82b166401
FT81x Manía <ft81xmania@gmail.com>
2021-10-04 14:18:15 -0500

    Add files via upload

--- examples/Touch_vector/BT817/BT817.ino
+++ examples/Touch_vector/BT817/BT817.ino
@@ -1,1 +1,89 @@
+#include <EEPROM.h>
+#include <GDT4Xv134.h>
 
+char TxT[50];
+int VTlimpio[50];
+int VTlleno[50];
+
+int Test =0;
+
+void setup() {
+  Serial.begin(9600);
+  //while (!Serial){}
+
+  for ( unsigned int i = 0 ; i < 25 ; i++ )
+  {
+   EEPROM.write(i, 0);
+   VTlimpio[i]=EEPROM.read(i);
+   Serial.print(i); Serial.print(": "); Serial.println(VTlimpio[i]); 
+  }
+
+  WriteVector();
+  
+  GD.begin();
+  
+MP();
+} void loop(){}
+
+void MP()
+{
+
+  for ( unsigned int i = 0 ; i < 25 ; i++ )
+  {
+   VTlleno[i]=EEPROM.read(i);
+   Serial.print(i); Serial.print(": "); Serial.println(VTlleno[i]); 
+  }
+
+while(1)
+ {
+  GD.ClearColorRGB(0x00aa00); 
+  GD.Clear();
+  GD.get_inputs(); 
+  GD.SaveContext();
+//  for ( unsigned int i = 0 ; i < 25 ; i++ )
+//  {
+//    sprintf(TxT, "Adress %d: %d", i,VTlimpio[i]);
+//    GD.cmd_text(60, 10+18*i, 28, OPT_CENTER, TxT);
+//  }
+
+  for ( unsigned int i = 0 ; i < 25 ; i++ )
+  {
+    sprintf(TxT, "Adress %d: %d", i,VTlleno[i]);
+    GD.cmd_text(60+200, 10+18*i, 28, OPT_CENTER, TxT);
+  }  
+  GD.RestoreContext();
+
+  GD.SaveContext();
+
+    GD.cmd_fgcolor(0x9025fa);   GD.ColorRGB(0xffffff);  
+    sprintf(TxT, "%d", Test);
+    GD.Tag(1);    GD.cmd_button(GD.w - 100, 150, 45, 55, 26, 0,  TxT);    GD.Tag(255);
+
+   if (GD.inputs.tag==1)
+       {
+         Test = Test + 1;
+       }    
+
+    GD.cmd_fgcolor(0x9025fa);   GD.ColorRGB(0xffffff);  
+    sprintf(TxT, "%d", Test);
+    GD.Tag(2);    GD.cmd_button(5, 5, 45, 55, 26, 0,  TxT);    GD.Tag(255);
+
+   if (GD.inputs.tag==2)
+       {
+         Test = Test - 1;
+       }        
+
+    GD.cmd_fgcolor(0x9025fa);   GD.ColorRGB(0xffffff);  
+    sprintf(TxT, "%d", Test);
+    GD.Tag(3);    GD.cmd_button((GD.w/2)-75, GD.h-60, 45, 55, 26, 0,  TxT);    GD.Tag(255);
+
+   if (GD.inputs.tag==3)
+       {
+         Test = Test + 2;
+       }        
+  
+  GD.RestoreContext();  
+
+  GD.swap();
+ }
+}
---
+++ examples/Touch_vector/BT817/Vector.ino
@@ -0,0 +1,28 @@
+void WriteVector()
+{
+  EEPROM.write(0, 1);     // VTlleno[0]=EEPROM.read(0);
+  EEPROM.write(1, 221);   // VTlleno[0]=EEPROM.read(1);
+  EEPROM.write(2, 161);   // VTlleno[0]=EEPROM.read(2);
+  EEPROM.write(3, 0);     // VTlleno[0]=EEPROM.read(3);
+  EEPROM.write(4, 0);     // VTlleno[0]=EEPROM.read(4);
+  EEPROM.write(5, 166);   // VTlleno[0]=EEPROM.read(5);
+  EEPROM.write(6, 1);     // VTlleno[0]=EEPROM.read(6);
+  EEPROM.write(7, 0);     // VTlleno[0]=EEPROM.read(7);
+  EEPROM.write(8, 0);     // VTlleno[0]=EEPROM.read(8);
+  EEPROM.write(9, 82);    // VTlleno[0]=EEPROM.read(9);
+  EEPROM.write(10, 85);   // VTlleno[0]=EEPROM.read(10);
+  EEPROM.write(11, 251);  // VTlleno[0]=EEPROM.read(11);
+  EEPROM.write(12, 255);  // VTlleno[0]=EEPROM.read(12);
+  EEPROM.write(13, 152);  // VTlleno[0]=EEPROM.read(13);
+  EEPROM.write(14, 252);  // VTlleno[0]=EEPROM.read(14);
+  EEPROM.write(15, 255);  // VTlleno[0]=EEPROM.read(15);
+  EEPROM.write(16, 255);  // VTlleno[0]=EEPROM.read(16);
+  EEPROM.write(17, 245);  // VTlleno[0]=EEPROM.read(17);
+  EEPROM.write(18, 166);  // VTlleno[0]=EEPROM.read(18);
+  EEPROM.write(19, 0);    // VTlleno[0]=EEPROM.read(19);
+  EEPROM.write(20, 0);    // VTlleno[0]=EEPROM.read(20);
+  EEPROM.write(21, 19);   // VTlleno[0]=EEPROM.read(21);
+  EEPROM.write(22, 186);  // VTlleno[0]=EEPROM.read(22);
+  EEPROM.write(23, 247);  // VTlleno[0]=EEPROM.read(23);
+  EEPROM.write(24, 255);  // VTlleno[0]=EEPROM.read(24);
+}
